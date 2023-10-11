3ea56356d82b0b7c19eb8a5689263233a3cb9254
rayandus <83055068+rayandus@users.noreply.github.com>
2023-10-11 13:37:39 +0800

    chore: sample port

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 import mongoose from 'mongoose';
 import { config } from 'common/config';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
+
+console.log('*** PORT', port);
 
 async function bootstrap() {
   // Opted not to use @nestjs/mongoose for now
