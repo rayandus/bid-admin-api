1e0d76880d9fa4a0be31635b4a6f1809b06716fd
rayandus <83055068+rayandus@users.noreply.github.com>
2023-10-11 15:01:44 +0800

    chore: try app service issue

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-    nvm install
-    nvm use
-    npm install -g pnpm
-    pnpm install
-    pnpm run build
+
+# Download and install NVM if it's not already installed
+if ! command -v nvm &> /dev/null
+then
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+fi
+
+# Install and use the latest version of Node.js
+nvm install
+nvm use
+
+# Install pnpm globally
+npm install -g pnpm
+
+# Install project dependencies and build the project
+pnpm install
+pnpm run build
